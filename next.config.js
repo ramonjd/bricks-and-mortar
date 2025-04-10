82eab9cf9feb2a49977f91ff879c142331953e7f
Ramon <ramonjd@gmail.com>
2025-04-10 22:28:45 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const withNextIntl = require('next-intl/plugin')('./src/lib/i18n/getTranslations
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = withNextIntl(nextConfig);const withNextIntl = require('next-intl/plugin')('./src/lib/i18n/getTranslations.ts');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
 module.exports = withNextIntl(nextConfig);
