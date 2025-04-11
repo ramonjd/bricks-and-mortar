0a3a0128c576c76941b88cce62e7abb9430b7c96
Ramon <ramonjd@gmail.com>
2025-04-11 19:45:57 +1000

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 const withNextIntl = require('next-intl/plugin')('./src/lib/i18n/getTranslations.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	env: {
+		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+		NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+	},
+	// Disable automatic static optimization for auth pages
+	async generateStaticParams() {
+		return {
+			'/*/auth/*': { dynamic: true },
+		};
+	},
+};
 
 module.exports = withNextIntl(nextConfig);
